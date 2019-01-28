0f50a4fae31559d1e4ea08c228db3044146b34b4
adi saputra <adii.empty@gmail.com>
2019-01-28 22:41:37 +0800

    adding function of return index

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index(){
-        return "Fungsi dari controller";
+        return view('product.index');
     }
 }
