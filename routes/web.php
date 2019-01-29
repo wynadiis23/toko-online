06d3dd66ea786141de302a4caa9e30d5bee77d38
adi saputra <adii.empty@gmail.com>
2019-01-29 15:47:47 +0800

    adding category routes

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,17 @@ Route::get('/hello', function() {
 });
 Route::get('/product', 'ProductController@index');
 Route::get('/show','ProductController@showAll');
+
+Route::group(["prefix"=>"/category"], function(){
+    Route::get("/", "CategoryController@index");
+
+    Route::get("/create", "CategoryController@create");
+    Route::post("/", "CategoryController@store");
+    
+    Route::get("/{id}", "CategoryController@show");
+
+    Route::get("/{id}/edit", "CategoryController@edit");
+    Route::put("/{id}", "CategoryController@update");
+
+    Route::delete("/{id}", "CategoryController@destroy");
+});
